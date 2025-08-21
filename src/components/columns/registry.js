1cd8ca44894899e1098a8973e3b6ff0161ad0735
Damidamida <119866689+Damidamida@users.noreply.github.com>
2025-08-21 06:50:23 +0300

    FixedDuplic

--- src/components/columns/registry.js
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_COLUMNS = [
-    { id: "task", title: "Задача", minPx: 50, resizable: true, fixedPosition: "first", visible: true, order: 0 },
-    { id: "dates", title: "Даты", minPx: 50, resizable: true, visible: true, order: 1 },
-    { id: "duration", title: "Длительность", minPx: 50, resizable: true, visible: true, order: 2 },
-    { id: "plus", title: "+", minPx: 50, resizable: false, fixedPx: 50, fixedPosition: "last", visible: true, order: 3 },
-];
